bac60dd2c54bfd54dbe0f1a50204b101498a243d
AbhyanshuSingh <124425348+AbhyanshuSingh@users.noreply.github.com>
2024-02-05 15:32:16 +0000

    03_convwesionOperations.js

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -31,5 +31,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+/**********************************  OPERATIONS *********************************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Abhyanshu"
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // prefix and postfix in js mdn study
