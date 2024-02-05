fe9728d5997b0e1d57bb43acb817923af030fbe0
AbhyanshuSingh <124425348+AbhyanshuSingh@users.noreply.github.com>
2024-02-05 15:07:22 +0000

    03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// let score = "33"
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = false
+let score = "Abhyanshu"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" - 33
+// "33abc" - NaN
+//  true - 1/ false - 0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Abhaysnhu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+//  1 - true; 0 - false
+// "" - false
+// "Abhaynshu" - true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
