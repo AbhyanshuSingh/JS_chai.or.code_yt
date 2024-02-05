3f6ed7ea6cb81d90f1a9218c6c9e193c21fcc6d8
AbhyanshuSingh <124425348+AbhyanshuSingh@users.noreply.github.com>
2024-02-05 14:37:14 +0000

    02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) only works in browser not in node
+
+let name = "Abhyanshu" 
+let age = 18 
+let isLoggedIn = false 
+let state
+
+// string("") datatype
+//number - ~ 2 to the power 53
+// bigint - for higher values
+//boolean - true/false
+// null - standalone value
+// undefined -
+// symbol - unique
+// object -
+
+
+console.log(typeof undefined);
+console.log(typeof null); // null is a object
